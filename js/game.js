414ffc4031e7ae0bc4c0bd9d5459b09a5a711e05
haokaili <>
2021-04-29 03:02:00 +0000

    phaser3. 2. 2

--- js/game.js
+++ js/game.js
@@ -18,7 +18,7 @@ const config = {
     }
   },
   // set background color
-  backgroundColor: 0x5f6e7a
+  backgroundColor: 0x5f6e7a,
   scale: {
     mode: Phaser.Scale.FIT,
     // we place it in the middle of the page.
