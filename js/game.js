972e4f8bcf3d20a1a1b84801e3af8c8dae526263
haokaili <>
2021-04-29 16:51:53 +0000

    added phaser 2

--- js/game.js
+++ js/game.js
@@ -28,3 +28,33 @@ const config = {
 
 const game = new Phaser.Game(config)
 console.log(game)
+/* global Phaser */
+
+// Copyright (c) 2021 haokai All rights reserved
+//
+// Created by: haokai
+// Created on: Apr 2021
+// This is the Phaser3 configuration file
+
+//* Game scene */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  // set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
