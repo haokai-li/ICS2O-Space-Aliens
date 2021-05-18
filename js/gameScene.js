6711d751ec83afcc8f6c541b1bfcacb059d077bd
haokaili <>
2021-05-18 21:32:05 +0000

    revise2 5.18

--- js/gameScene.js
+++ js/gameScene.js
@@ -35,10 +35,10 @@ class GameScene extends Phaser.Scene {
 
   update (time, delta) {
     // called 60 times a second, hopefully!
-    
+
     const keyLeftObj = this.input.keyboard.addKey('LEFT')
     const keyRightObj = this.input.keyboard.addKey('RIGHT')
-    
+
     if (keyLeftObj.isDown === true) {
       this.ship.x -= 15
       if (this.ship.x < 0) {
