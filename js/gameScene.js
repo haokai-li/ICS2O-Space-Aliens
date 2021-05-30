358c2029d3bd3535e613b3ffb9b74e38e05b98d8
haokaili <>
2021-05-30 22:49:07 +0000

    revise 5.30

--- js/gameScene.js
+++ js/gameScene.js
@@ -55,16 +55,16 @@ class GameScene extends Phaser.Scene {
     // create a group for the aliens
     this.alienGroup = this.add.group()
     this.createAlien()
-    
+
     // Colliosions between missiles and aliens
-    this.physics.add.collider(this.missileGroup, this.alienGroup, function(missileCollide, alienCollide) {
+    this.physics.add.collider(this.missileGroup, this.alienGroup, function (missileCollide, alienCollide) {
       alienCollide.destroy()
       missileCollide.destroy()
       this.sound.play('explosion')
       this.createAlien()
       this.createAlien()
     }.bind(this))
-    }
+  }
 
   update (time, delta) {
     // called 60 times a second, hopefully!
