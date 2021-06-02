8a8e2537453d8903855d2cf427fa51e72c30a89a
haokaili <>
2021-06-02 21:37:05 +0000

    revise 6.2

--- js/gameScene.js
+++ js/gameScene.js
@@ -26,7 +26,7 @@ class GameScene extends Phaser.Scene {
     this.fireMissle = false
     this.score = 0
     this.scoreText = null
-    this.scoreTextStyle = { font: '65px Arial', fill: '#ffffff', align: 'center'}
+    this.scoreTextStyle = { font: '65px Arial', fill: '#ffffff', align: 'center' }
   }
 
   init (date) {
@@ -50,7 +50,7 @@ class GameScene extends Phaser.Scene {
     this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
     this.background.setOrigin(0, 0)
 
-    this.scoreText = this.add.text(10, 10, "Score: " + this.score.toString(), this.scoreTextStyle)
+    this.scoreText = this.add.text(10, 10, 'Score: ' + this.score.toString(), this.scoreTextStyle)
 
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
 
